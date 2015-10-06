257dbb7dca2b5178b06d67c4f9bb17041f4a5df7
Yuval Saraf <yuvalsaraf@gmail.com>
2015-10-06 09:20:35 +0300

    Fix workflow of hidden Login, also fix todo css for buttons

--- src/containers/AppRoute.jsx
+++ src/containers/AppRoute.jsx
@@ -8,31 +8,8 @@ export default class AppRoute extends Component {
   static propTypes = {
     isLoggedIn: PropTypes.bool
   };
-
   defaultProps() {
     return {
       isLoggedIn: false
     };
   }
-  render() {
-    let component;
-
-    if (this.props.isLoggedIn) {
-      component = (
-        <Router history={createHistory()}>
-      	  <Route path="/main" component={TodoApp}/>
-          <Route path="/login" component={Login}/>
-      	  <Redirect from="*" to="/main"/>
-        </Router>
-      );
-    } else {
-      component = (
-        <Router history={createHistory()}>
-      	  <Route path="/login" component={Login}/>
-          <Redirect from="*" to="/login"/>
-        </Router>
-      );
-    }
-    return component;
-  }
-}
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,15 +1,4 @@
 import React, { Component } from 'react';
 
-export default class Login extends Component {
   render() {
     return (
-      <div>
-        <h1>Login</h1>
-        <input type="text" placeholder="Email"/>
-        <input type="password" placeholder="Password"/>
-      </div>
-    );
-  }
-}
-
-export default connect(state => ({ credentials: state.credentials }), dispatch => ({ credentialsActions: bindActionCreators(CredentialsActions, dispatch) }))(Login);

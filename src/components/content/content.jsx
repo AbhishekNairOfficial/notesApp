70e98ff725a93bb85fc82f05c9d729c52278ecb0
Abhishek P <abhishek@moonraft.com>
2018-04-22 17:24:22 +0530

    Initial Commit

---
+++ src/components/content/content.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './content.css';
+
+export default class Content extends Component {
+  render() {
+    return (
+      <div className="content">
+        heya
+      </div>
+    );
+  }
+}
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <span className="title">Notes</span>
+      </div>
+    );
+  }
+}

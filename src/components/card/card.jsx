6208b5924a93ece13b87027445f8f9d3b6a24c13
Abhishek P <abhishek@moonraft.com>
2018-05-05 21:32:11 +0530

    intermittent changes

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -8,13 +8,50 @@ export default class Card extends Component {
   render() {
     return (
         <div className="card">
-          <span className="card-title">
-            {this.props.data.name}
-          </span>
-          <span className="card-description">
-            {this.props.data.description}
-          </span>
+          <div className="card-top">
+            <div className="card-attachments">
+              <i className={this.isAttached("image")}>insert_photo</i>
+              <i className={this.isAttached("video")}>play_arrow</i>
+              <i className={this.isAttached("file")}>attach_file</i>
+            </div>
+          </div>
+
+          <div className="card-content">
+            <span className="card-title">
+              {this.props.data.name}
+            </span>
+            <div className="card-description">
+              {this.props.data.description}
+            </div>
+          </div>
+
+          <div className="card-bottom"></div>
         </div>
     );
+  }
+
+  isAttached(type) {
+    switch (type) {
+    case "image":
+      if (this.props.data.image) {
+        return "material-icons";
+      } else {
+        return "material-icons greyed";
+      }
+      break;
+    case "video":
+      if (this.props.data.video) {
+        return "material-icons";
+      } else {
+        return "material-icons greyed";
+      }
+      break;
+    case "file":
+    if (this.props.data.file) {
+        return "material-icons";
+      } else {
+        return "material-icons greyed";
+      }
+}
   }
 }

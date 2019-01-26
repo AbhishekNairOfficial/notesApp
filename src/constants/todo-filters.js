642cae523cae295907c04f1034e63a9d6a1e5359
Abhishek Nair <abhisheknr4@gmail.com>
2019-01-26 13:27:56 +0530

    Fresh Start

--- src/constants/todo-filters.js
+++
@@ -1,3 +0,0 @@
-export const SHOW_ALL = 0;
-export const SHOW_MARKED = 1;
-export const SHOW_UNMARKED = 2;
--- test/mock/react-redux.js
+++
@@ -1,7 +0,0 @@
-import { stub } from 'sinon';
-
-const getReactReduxMock = () => ({
-  connect: stub().returns(component => component)
-});
-
-export default getReactReduxMock;
